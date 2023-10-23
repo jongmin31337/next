21838b3a219a198cf75edd951df3380b5720b86d
jjong31337 <ljm31337@gmail.com>
2023-10-24 07:50:54 +0900

    생활코딩 - next(csr 구현)

---
+++ nextapp/src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+   <>
+    <h2>Welcome</h2>
+      Hello, Web!
+      <img src="/test.jpg"></img>
+   </>
+  )
+}
---
+++ nextapp/src/app/read/[id]/page.js
@@ -0,0 +1,11 @@
+export default function Read(props) {
+    return (
+        <>
+            <h2>Read</h2>
+            <div>
+            {props.params.id}
+            </div>
+        </>
+    );
+}
+
