26a5eb6810dd38ac409c9f1b2c632f279ac217d1
jjong31337 <ljm31337@gmail.com>
2023-11-05 17:25:35 +0900

    key 추가

--- note/app/products/page.tsx
+++ note/app/products/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
-export default function Products() {
-    const products = ["shirt", "pants", "skirt", "shoes"];
+const products = ["shirt", "pants", "skirt", "shoes"];
+
+export default function ProductsPage() {
     
     return (
         <>
             <h1>제품 소개 페이지!</h1>
             <ul>
-                {products.map((product) => <Link href={`/products/${product}`}><li>{product}</li></Link>)}
+                {products.map((product, index) => <li key={index}><Link href={`/products/${product}`}>{product}</Link></li>)}
             </ul>
         </>
 
