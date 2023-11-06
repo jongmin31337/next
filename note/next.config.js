a4cdc78b5900de56dfa490f4b2c61b8daabd5da7
jjong31337 <ljm31337@gmail.com>
2023-11-06 16:48:01 +0900

    redirect 설정 해보기

--- note/next.config.js
+++ note/next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
                 hostname: 'images.unsplash.com',
             }
         ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/products/deleted_forever',
+                destination: '/products',
+                permanent: true, // 이 source는 destination으로 이동했으니까 너는 안심하고 캐시해도되.. 임시적일때는 false가 좋음
+            }
+        ]
     }
 }
 
