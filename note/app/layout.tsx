8fab966f623f488e5a92d0e8c7a42b62a01be5e4
jjong31337 <ljm31337@gmail.com>
2023-11-06 16:43:40 +0900

    폰트 설정 방법

--- note/app/layout.tsx
+++ note/app/layout.tsx
@@ -1,7 +1,16 @@
 import Link from 'next/link';
 import styles from './layout.module.css';
 import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google';
 
+const inter = Inter({subsets: ['latin']});
+const sans = Open_Sans({subsets: ['latin']});
+const gothic = Nanum_Gothic({
+  weight: '700',
+  subsets: ['latin']
+})
 
 export const metadata : Metadata = {
   title: '멋진 제품 사이트',
@@ -17,10 +26,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h1>Demo Note App</h1>
+          <h1 className={gothic.className}>Demo Note App</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/about">About</Link>
