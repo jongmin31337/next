db1df6cf043e2c593dd9c51fa861f12ab8164a2d
jjong31337 <ljm31337@gmail.com>
2023-11-06 16:35:34 +0900

    Image 등록법

--- note/app/products/[slug]/page.tsx
+++ note/app/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import ImageLoader from "../../components/ImageLoader";
 
 
 type Props = {
@@ -23,7 +25,12 @@ export default async function ProductPage({params: {slug}}: Props) {
     }
 
     // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-    return <h1>{product.name} 제품 설명 페이지!</h1>
+    return (
+        <>
+            <h1>{product.name} 제품 설명 페이지!</h1>
+            <Image src={`/images/${product.image}`} alt={product.name} width={400} height={400} />
+        </>
+    )
 }
 
 export async function generateStaticParams() {
