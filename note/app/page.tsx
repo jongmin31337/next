2605ab3202f00a4b76eea02a1cd683dcce2098d3
jjong31337 <ljm31337@gmail.com>
2023-11-07 09:52:25 +0900

    ver2

--- note/app/page.tsx
+++ note/app/page.tsx
@@ -5,7 +5,7 @@ export const revalidate = 60;
 export default function Home() {
   return (
     <>
-      <h1>홈페이지</h1>
+      <h1>홈페이지 버전 2</h1>
       <Counter />
       <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400} />
     </>
