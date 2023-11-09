bfa02e5fe30c1daf1497f7fa7c4f6c983be752bf
jjong31337 <ljm31337@gmail.com>
2023-11-09 16:53:31 +0900

    header nav 작업중

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link';
-
+import Image from 'next/image';
+import './global.css'
+import logo from '/public/images/logo.png';
+import { FaBars } from 'react-icons/fa';
 
 export default function RootLayout({
     children,
@@ -9,6 +11,25 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
+            <nav className="flex justify-between items-center m-auto w-full xl:w-1280 max-md:flex-col max-md:items-start max-md:px-2 max-md:py-4">
+                <div className="flex">
+                    {/* //<img src="logo.png" alt=""> */}
+                    <Image src={logo} alt='logo' />
+                    <h2 className='p-15 font-NatoSansMidum text-3xl'>쫑의 기술블로그</h2>
+                </div>
+
+                <ul className="flex p-0 max-md:hidden max-md:flex-col max-md:items-center max-md:w-full">
+                    <li className='px-2 py-4 text-xl hover:font-bold'><a href="">Project</a></li>
+                    <li className='px-2 py-4 text-xl hover:font-bold'><a href="">Tech</a></li>
+                    <li className='px-2 py-4 text-xl hover:font-bold'><a href="">Tech</a></li>
+                    <li className='px-2 py-4 text-xl hover:font-bold'><a href="">Tech</a></li>
+                    <li className='px-2 py-4 text-xl hover:font-bold'><a href="">Tech</a></li>
+                </ul>
+
+                <a href="" className='hidden absolute right-2 text-2xl max-md:block'>
+                    <FaBars/>
+                </a>
+            </nav>
             {children}
         </body>
       </html>
--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -3,7 +3,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1280': '1280px',
+      },
+    },
+    fontFamily: {
+      NatoSansMidum: ["NatoSans-Medium"],
+    },
+    screens: {
+      'max-md': {'max': '767px'},
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
