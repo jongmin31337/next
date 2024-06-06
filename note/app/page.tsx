655e3f40bbcc8af65626c549c180a8930909a2dd
jjong31337 <ljm31337@gmail.com>
2024-06-06 22:06:10 +0900

    Feat. WebGl Test

--- note/app/page.tsx
+++ note/app/page.tsx
@@ -1,6 +1,20 @@
+'use client'
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+
 export default function Home() {
-  return (
-    <>
-    </>
-  )
+  const { unityProvider } = useUnityContext({
+    // loaderUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.loader.js",
+    // dataUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.data",
+    // frameworkUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.framework.js",
+    // codeUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.wasm",
+        loaderUrl: "/webgl/Test.loader.js",
+    dataUrl: "/webgl/Test.data",
+    frameworkUrl: "/webgl/Test.framework.js",
+    codeUrl: "/webgl/Test.wasm",
+  });
+
+
+  return <Unity unityProvider={unityProvider} />;
+
 }
