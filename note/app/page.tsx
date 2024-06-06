e84559c927248175778a96e735926a78c5e49397
jjong31337 <ljm31337@gmail.com>
2024-06-06 21:43:57 +0900

    Feat. Vercel Test

--- note/app/page.tsx
+++ note/app/page.tsx
@@ -1,13 +1,6 @@
-import Image from 'next/image';
-import Counter from './components/Counter';
-export const revalidate = 60;
-
 export default function Home() {
   return (
     <>
-      <h1>홈페이지 버전 2</h1>
-      <Counter />
-      <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400} />
     </>
   )
 }
