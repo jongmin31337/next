7e0627e7f303e67c878633321a6918c9ec526fba
jjong31337 <ljm31337@gmail.com>
2024-06-06 23:30:23 +0900

    Feat. URL CHANGE

--- note/app/page.tsx
+++ note/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
     // dataUrl: "https://github.com/jongmin31337/testtest/blob/main/Terrain_Mountains_1024.data",
     // frameworkUrl: "https://github.com/jongmin31337/testtest/blob/main/Terrain_Mountains_1024.framework.js",
     // codeUrl: "https://github.com/jongmin31337/testtest/blob/main/Terrain_Mountains_1024.wasm",
-    loaderUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.loader.js",
-    dataUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.data",
-    frameworkUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.framework.js",
-    codeUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.wasm",
-    // loaderUrl: "/webgl/Terrain_Mountains_1024.loader.js",
-    // dataUrl: "/webgl/Terrain_Mountains_1024.data",
-    // frameworkUrl: "/webgl/Terrain_Mountains_1024.framework.js",
-    // codeUrl: "/webgl/Terrain_Mountains_1024.wasm",
+    // loaderUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.loader.js",
+    // dataUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.data",
+    // frameworkUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.framework.js",
+    // codeUrl: "https://cdn.golfday.uneekor.com/test/GolfCourse_URP_0524.wasm",
+    loaderUrl: "/webgl/Terrain_Mountains_1024.loader.js",
+    dataUrl: "/webgl/Terrain_Mountains_1024.data",
+    frameworkUrl: "/webgl/Terrain_Mountains_1024.framework.js",
+    codeUrl: "/webgl/Terrain_Mountains_1024.wasm",
   });
 
 
